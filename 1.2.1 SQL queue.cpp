cfb52daed68c44e6a594d4c3073da4c49ee55f0a
Li-can-cheng <b19357160114@gmail.com>
2023-10-10 11:13:30 +0800

    2023-10-10

---
+++ 1.2.1 SQL queue.cpp
@@ -0,0 +1,29 @@
+
+//实例：
+//3 7
+//1 2 1 5 4 4
+
+#include <iostream>
+#include <queue>
+
+using namespace std;
+int Hash[1003]={0};//哈希检查内存中是否存有单词，若为1则存在
+queue <int> mem;//队列模拟内存
+int main(){
+    int m,n,cnt=0;
+    cin>>m>>n;
+    while(n--){
+        int en;cin>>en;
+        if(!Hash[en]){//如果没查到
+            ++cnt;//增加查找次数
+            mem.push(en);//内存队列中加入en
+            Hash[en]=1;//标记内存中存在的en
+            while(mem.size()>m){//若超出内存
+                Hash[mem.front()]=0;
+                mem.pop();//删除内存第一个内容并且去除标记
+            }
+        }
+    }
+    cout<<cnt;
+    return 0;
+}
