4b8ce7ab524887a4e73c1e9eac7ca64a6ec89cf0
Li-can-cheng <b19357160114@gmail.com>
2023-10-22 14:56:13 +0800

    2023-10-22

---
+++ 1.4.3哈夫曼.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include <queue>
+#include <algorithm>
+
+using namespace std;
+int main(){
+    priority_queue<int,vector<int>,greater<int>>q;
+    string s;
+    while(getline(cin,s)&&s!="END"){
+        //AAAAABCD
+        sort(s.begin(),s.end());//让一样的放在一起。
+        int num=1;
+        for(int i=1;i<=s.length();i++){
+            if(s[i]!=s[i-1]){q.push(num);num=1;}
+            else num++;
+        }
+        int ans=0;
+        //AAAAA
+        if(q.size()==1) ans=s.length();
+        while(q.size()>1){
+            int a=q.top();q.pop();
+            int b=q.top();q.pop();
+            q.push(a+b);
+            ans+=a+b;
+        }
+        q.pop();
+        printf("%d %d %.1lf\n",s.length()*8,ans
+               ,s.length()*8/(double)ans);
+    }
+    return 0;
+}
